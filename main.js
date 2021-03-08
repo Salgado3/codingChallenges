580b23cb6576327a37e5a9ecd710ffceb6a2d12c
Jaime Salgado <jsalg10@gmail.com>
2021-03-07 19:25:55 -0600

    finished another codewars

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 
 // Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string). 
-// String ends with?
+// String ends with? (3/7/2021)
 
 function solution(str, ending){
  
     return str.endsWith(ending)
     }
+
+    // Very simple, given a number, find its opposite.
+
+    function opposite(number) {
+        return -(number)
+      }
