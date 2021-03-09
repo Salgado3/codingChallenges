3e89a4fc0555b9773fac6dc4a9b83b91ac0f559a
Jaime Salgado <jsalg10@gmail.com>
2021-03-08 23:21:48 -0600

    solved

--- main.js
+++ main.js
@@ -38,3 +38,11 @@ class SmallestIntegerFinder {
   }
 
 //   March 8th
+
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+function squareSum(numbers){
+    return numbers.reduce((a,b)=> a+ b**2, 0)
+    }
