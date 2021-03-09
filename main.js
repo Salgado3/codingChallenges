b5e03d012a67d18f91e211e309349cf21ec90a16
Jaime Salgado <jsalg10@gmail.com>
2021-03-08 23:51:26 -0600

    updated file

--- main.js
+++ main.js
@@ -37,27 +37,5 @@ class SmallestIntegerFinder {
     }
   }
 
-//   March 8th
 
-// Complete the square sum function so that it squares each number passed into it and then sums the results together.
-
-// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
-
-// function squareSum(numbers){
-//     return numbers.reduce((a,b)=> a+ b**2, 0)
-//     }
-
-//     Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
-
-// For example,
-
-// [true,  true,  true,  false,
-//   true,  true,  true,  true ,
-//   true,  false, true,  false,
-//   true,  false, false, true ,
-//   true,  true,  true,  true ,
-//   false, false, true,  true]
-function countSheeps(arrayOfSheep) {
-    return arrayOfSheep.filter(Boolean).length;
-  }
   
--- main1.js
+++ main1.js
@@ -1,22 +1,24 @@
-//Create an espresso machine constructor that makes machines with 4 properties and 3 methods
 
-class ExpressoMachine{
-constructor (brand, cost, temp, grinder, brew, steam, grinding){
-this.brand = brand
-this.cost = cost
-this.temp = temp
-this.grinder = grinder
+//   March 8th
 
-brew() {
-console.log("let's get a brewin")
-}
-steam() {
-    console.log("It's getting steamy in here")
-}
-grinding() {
-    console.log("from ghetto to ghetto to backyard to yard")
-}
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
 
-}
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
-let gaggia = new ExpressoMachine()
+function squareSum(numbers){
+    return numbers.reduce((a,b)=> a+ b**2, 0)
+    }
+
+//     Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+function countSheeps(arrayOfSheep) {
+    return arrayOfSheep.filter(Boolean).length;
+  }
