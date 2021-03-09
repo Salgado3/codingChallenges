b3a401b558dcb60aedb88dd1ddf06ba05d8595fd
Jaime Salgado <jsalg10@gmail.com>
2021-03-08 23:48:10 -0600

    pretty hard

--- main.js
+++ main.js
@@ -43,6 +43,21 @@ class SmallestIntegerFinder {
 
 // For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
-function squareSum(numbers){
-    return numbers.reduce((a,b)=> a+ b**2, 0)
-    }
+// function squareSum(numbers){
+//     return numbers.reduce((a,b)=> a+ b**2, 0)
+//     }
+
+//     Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+function countSheeps(arrayOfSheep) {
+    return arrayOfSheep.filter(Boolean).length;
+  }
+  
