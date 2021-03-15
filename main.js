b4d618a694db7308e56c529d35183cf48c5ffa07
Jaime Salgado <jsalg10@gmail.com>
2021-03-14 21:41:05 -0500

    eval() problem.

--- main.js
+++ main.js
@@ -89,3 +89,16 @@ function litres(time) {
   let waterPerHour = Math.floor(time / 2)
   return waterPerHour;
 }
+
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+// Examples
+
+// basicOp('+', 4, 7)         // Output: 11
+// basicOp('-', 15, 18)       // Output: -3
+// basicOp('*', 5, 5)         // Output: 25
+// basicOp('/', 49, 7)        // Output: 7
+function basicOp(operation, value1, value2)
+{return eval(value1 + operation + value2)}
