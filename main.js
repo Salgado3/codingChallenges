217d2138d25dbd4aed1270b4beae457f0f88de37
Jaime Salgado <jsalg10@gmail.com>
2021-03-17 23:00:04 -0500

    updated file

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 // Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string). 
-// String ends with? (3/7/2021)
+// String ends with? (3/7/2021).
 
 function solution(str, ending){
  
