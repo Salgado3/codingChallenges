23d3b792d49d17314cb09f49995134cf4ef9a597
Jaime Salgado <jsalg10@gmail.com>
2021-04-07 19:00:23 -0500

    completed daily challenge

--- main.js
+++ main.js
@@ -202,3 +202,28 @@ function reverseWords(str) {
     }).join(' ');
   }
 
+//April 7 2020
+// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+// #Examples:
+
+// Kata.getMiddle("test") should return "es"
+
+// Kata.getMiddle("testing") should return "t"
+
+// Kata.getMiddle("middle") should return "dd"
+
+// Kata.getMiddle("A") should return "A"
+
+//why it works: we are putting Math floor to get whole number. Then we are saying if it is even, then take the two middle numbers, if it is odd then just take the middle element
+function getMiddle(s)
+
+  {
+  let middle = Math.floor(s.length/2);
+  
+  return s.length % 2 === 0
+        ? s.slice(middle-1, middle+1)
+        : s.slice(middle, middle+1);
+  
+}
+
