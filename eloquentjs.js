b12dcb65b82aceabd3577d0928644ad8c1056ee2
Jaime Salgado <jsalg10@gmail.com>
2021-04-22 10:48:14 -0500

    solved fizzbuzz chap 2

--- eloquentjs.js
+++ eloquentjs.js
@@ -1,4 +1,4 @@
-// Chapter 2
+// Chapter 2 4/22
 // Write a loop that makes seven calls to console.log to output the following triangle:
 
 // #
@@ -18,3 +18,16 @@ console.log(line)
 // When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
 
 // (This is actually an interview question that has been claimed to weed out a significant percentage of programmer candidates. So if you solved it, your labor market value just went up.)
+
+
+for(let i=0;i <=100; i++){
+    if(i%3 == 0 && i%5 != 0){
+        console.log("fizz")
+    }else if(i%5 == 0 && i%3 != 0){
+        console.log("buzz")
+    }else if(i%5 == 0 && i%3 == 0){
+        console.log("fizzbuzz")
+    }else{
+        console.log(i)
+    }
+}
