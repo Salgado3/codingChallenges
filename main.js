a4fca19fae01512055456c1a0cb197e73db9dce3
Jaime Salgado <jsalg10@gmail.com>
2021-04-24 11:18:48 -0500

    code for the day

--- main.js
+++ main.js
@@ -309,4 +309,4 @@ function apple(x){
     return Math.pow(x, 2) > 1000 ? 'It\'s hotter than the sun!!' : 'Help yourself to a honeycomb Yorkie for the glovebox.' ;
   }
 
-  funcrion andriod ()
+  function andriod ()
