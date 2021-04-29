86b2d8fb9cb84aafb7108ea60a3d6bc3ed2f79c3
Jaime Salgado <jsalg10@gmail.com>
2021-04-29 12:29:32 -0500

    completed number 3 for arrays

--- main.js
+++ main.js
@@ -372,3 +372,40 @@ function smallEnough(arr, limit){
   return arr.every(arr => arr <= limit) 
   }
 
+  // or
+  function smallEnough(array, limit){
+    return array.filter( (value) => value > limit ).length > 0 ? false : true;
+}
+
+// (Array 2 of 5)
+// JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+// The solution would work like the following:
+function getEvenNumbers(numArray){
+  return numArray.filter(x=> x%2 == 0)
+}
+// (Array 3 of 5)
+// Task:
+
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+// Examples:
+
+// Input: [0]
+// Output: "even"
+
+// Input: [0, 1, 4]
+// Output: "odd"
+
+// Input: [0, -1, -5]
+// Output: "even"
+function oddOrEven(arr) {
+  return arr.reduce((a,b)=> a+b,0)%2 ==0 ? "even" : "odd"
+   }
+  
+   
+   
+ 
