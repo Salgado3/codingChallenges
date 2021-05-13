df50f0df95f7e5ef659c9f7cba23128bd020d3ec
Jaime Salgado <jsalg10@gmail.com>
2021-05-13 13:55:23 -0500

    coding challenge completed

---
+++ arraymethods.md
@@ -0,0 +1,14 @@
+Please research the following methods:
+map
+reduce
+filter
+forEach
+sort
+slice
+pop
+shift
+push
+unshift
+includes
+indexOf
+every
--- main.js
+++ main.js
@@ -483,4 +483,26 @@ const rmHighLow = arr.sort((a,b)=> a-b).pop().shift()
 const sum = rmHighLow.map((a,b)=> a+b)
 return sum
 }
-// May 12th
+// May 13th 
+// Create a function that takes 2 positive integers in form of a string as an input, and outputs the sum (also as a string):
+
+// Example: (Input1, Input2 -->Output)
+
+// "4",  "5" --> "9"
+// "34", "5" --> "39"
+
+// Notes:
+
+//     If either input is an empty string, consider it as zero.
+
+function sumStr(a,b) {
+  let sum = Number(a) + Number(b)
+  return sum.toString()
+  }
+
+  // or
+
+  function sumStr(a,b) {
+    return String(Number(a)+Number(b));
+  }
+
