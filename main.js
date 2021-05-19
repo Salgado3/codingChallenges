485018eac8de7e0b8e9fe026a86f9df5dc1229a5
Jaime Salgado <jsalg10@gmail.com>
2021-05-18 23:13:54 -0500

    daily challenge

--- main.js
+++ main.js
@@ -619,3 +619,14 @@ var isSquare = function(n){
 var isSquare = function(n){
   return Number.isInteger(Math.sqrt(n));
 }
+
+// May 18th
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+// Example:
+
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+function reverseWords(str){
+  return str.split(' ').reverse().join(" ")
+}
