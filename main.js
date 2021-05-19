9796c848089d5f4829e3fae11e1c3e2c3a476b16
Jaime Salgado <jsalg10@gmail.com>
2021-05-18 23:20:37 -0500

    second challenge

--- main.js
+++ main.js
@@ -630,3 +630,18 @@ var isSquare = function(n){
 function reverseWords(str){
   return str.split(' ').reverse().join(" ")
 }
+
+// In this kata, you will do addition and subtraction on a given string. The return value must be also a string.
+
+// Note: the input will not be empty.
+// Examples
+
+// "1plus2plus3plus4"  --> "10"
+// "1plus2plus3minus4" -->  "2"
+
+
+
+function calculate(str) {
+  return eval(str.split("plus").join("+").split("minus").join("-")).toString();
+
+}
