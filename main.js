04e049d78c7c5d6dc3747ffa0680617953d03583
Jaime Salgado <jsalg10@gmail.com>
2021-05-21 00:51:54 -0500

    daily challenge number 2 string

--- main.js
+++ main.js
@@ -620,7 +620,7 @@ var isSquare = function(n){
   return Number.isInteger(Math.sqrt(n));
 }
 
-// May 18th
+// May 18th (String 1 for due date may25th)
 // Complete the solution so that it reverses all of the words within the string passed in.
 
 // Example:
@@ -651,4 +651,25 @@ function calculate(str) {
 function calculate(str) {
 
   return eval(str.replace(/plus/gi, '+').replace(/minus/gi, '-')).toString();
+}
+
+
+// May21st (String 2 for due date may25th)
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+// Example:
+
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+
+// Notes:
+
+//     All numbers are valid Int32, no need to validate them.
+//     There will always be at least one number in the input string.
+//     Output string must be two numbers separated by a single space, and highest number is first.
+
+function highAndLow(numbers){
+  numbers = numbers.split(' ');
+  return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
 }
