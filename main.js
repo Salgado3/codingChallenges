5501fa82797abdcc8316a7423124180a840ec134
Jaime Salgado <jsalg10@gmail.com>
2021-05-21 07:26:27 -0500

    daily coding challenge

--- main.js
+++ main.js
@@ -673,3 +673,20 @@ function highAndLow(numbers){
   numbers = numbers.split(' ');
   return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
 }
+// May21st (String 3 for due date may25th)
+// Complete the solution so that the function will break up camel casing, using a space between words.
+// Example
+
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+function solution(string) {
+  string = string.split('').map(function (el) {
+    if (el === el.toUpperCase()) {
+      el = ' ' + el
+    }
+    return el
+  })
+  return string.join('')
+}
