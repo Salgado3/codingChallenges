63f6d107165408f2422f7a3ef90e7e19f9979b8c
Jaime Salgado <jsalg10@gmail.com>
2021-06-27 23:00:46 -0500

    challenge for loop

--- codeWars.js
+++ codeWars.js
@@ -828,3 +828,20 @@ function getCount(str) {
   return vowelCount === null ? 0 : vowelCount.length
   
 }
+
+
+// or 
+
+function getCount(str) {
+  var vowelsCount = 0;
+  var vowels = ["a","e","i","o","u"];
+  for(var i = 0;i < str.length;i++){
+    for(var j=0;j<vowels.length;j++){
+      if(str[i] === vowels[j]){
+        vowelsCount++;
+      }
+    }
+  }
+  
+  return vowelsCount;
+}
