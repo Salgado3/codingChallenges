b0bc71b869c5409ba1803cf21419d7791b59f218
Jaime Salgado <jsalg10@gmail.com>
2021-07-06 09:49:01 -0500

    daily challenge

--- codeWars.js
+++ codeWars.js
@@ -958,3 +958,23 @@ function likes(names) {
 function squareDigits(num){
   return Number(('' + num).split('').map(a => (a * a)).join(''));
     }
+
+    //July 6th 
+
+//     If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+//     Note: If the number is a multiple of both 3 and 5, only count it once. Also, if a number is negative, return 0(for languages that do have them)
+
+function solution(number){
+  var sum = 0;
+  for (let i=1; i < number; i++){
+  if (i%3 == 0 || i%5 == 0){
+    sum+=i
+  
+    
+    
+  }}
+ return sum 
+}
