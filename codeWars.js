56f4efa80537841dae748b5474fc551e7ee3f433
Jaime Salgado <jsalg10@gmail.com>
2021-07-10 07:48:40 -0500

    decending order

--- codeWars.js
+++ codeWars.js
@@ -1123,3 +1123,18 @@ function findShort(str){
 
 
 console.log(findShort("bitcoin take over the world maybe who knows perhaps"))
+
+// //Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+// Examples:
+
+// Input: 42145 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 123456789 Output: 987654321
+
+function descendingOrder(n){
+  return parseInt(n.toString().split('').sort().reverse().join(''), 10);
+}
+
+console.log(descendingOrder(12434367))
