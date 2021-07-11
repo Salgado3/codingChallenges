82a4acd1e30d2ab877ada066f2d10d42e8a03922
Jaime Salgado <jsalg10@gmail.com>
2021-07-10 22:00:27 -0500

    daily challenges

--- codeWars.js
+++ codeWars.js
@@ -1138,3 +1138,84 @@ function descendingOrder(n){
 }
 
 console.log(descendingOrder(12434367))
+// Please make sure everyone at your table understands reduce before you start! Given a list of integers, determine whether the sum of its elements is odd or even. Give your answer as a string matching "odd" or "even". If the input array is empty consider it as: [0] (array with a zero). Examples: Input: [0] Output: "even" Input: [0, 1, 4] Output: "odd" Input: [0, -1, -5] Output: "even"
+
+
+// Params 
+
+//always will have an array. if empty, treat as a 0
+//whole numbers, both negative and positive
+
+// Return 
+
+//result will be string of "odd" or "even"
+//determine if the SUM is odd or even number
+// Examples
+
+console.log(sumOddOrEven([1,2,3]), 6, "even")
+console.log(sumOddOrEven([1,2,3,1]), 7, "odd")
+console.log(sumOddOrEven([1,2,3,-1]), 5, "odd")
+console.log(sumOddOrEven([]), 0, "even")
+// Pseudocode
+
+function sumOddOrEven (arr) {
+//obtain the sum of the array 
+ let sum = arr.reduce((a,c)=>a+c,0) 
+// confirm if the sum is odd or even
+return (sum%2 == 0) ? "even":"odd"
+//return string of "even" or "odd"
+
+
+//_____________________________________________
+// Remove the spaces from the string, then return the resultant string. One solution must use filter!
+
+let x = 'Hello World'
+let y = 'Remove the spaces'
+
+//Answer No filter
+function RemoveSpaces (str) {
+  return str.replace(/ /g, '') //Replaces all
+  //str.replace(' ', '') //Replaces only the first space
+}
+console.log(RemoveSpaces(x))
+console.log(RemoveSpaces(y))
+
+console.log('\nWith Filter')
+//With Filter
+function RemoveSpacesWithFilter (str) {
+  return str.split('').filter(word => word !== ' ').join('')
+}
+console.log(RemoveSpacesWithFilter(x))
+console.log(RemoveSpacesWithFilter(y))
+
+console.log('\nWith Iterator')
+function RemoveSpacesWithIterator (str) {
+  let ans = ''
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== ' ') {
+      ans += str[i]
+    }
+  }
+  return ans
+}
+console.log(RemoveSpacesWithIterator(y))
+
+//Daily challenge 
+
+// In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
+
+//     make as few changes as possible.
+//     if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
+
+// For example:
+
+// solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to lowercase.
+// solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
+// solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
+
+
+function solve (str) {
+  return [...str].filter(letter => letter === letter.toUpperCase()).length > str.length / 2 ?
+    str.toUpperCase() :
+    str.toLowerCase()
+}
