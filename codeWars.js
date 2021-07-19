e9ea92bf54b82566887b37c12d828304648e9b43
Jaime Salgado <jsalg10@gmail.com>
2021-07-19 13:33:29 -0500

    daily challenge

--- codeWars.js
+++ codeWars.js
@@ -1496,3 +1496,19 @@ function greetDevelopers(list) {
 function accum(s) {
 	return [...s].map((e,i)=> e.toUpperCase()+ e.repeat(i).toLowerCase()).join("-")
 }
+
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+const XO = str => {
+  str = str.toLowerCase().split('');
+  return str.filter(x => x === 'x').length  === str.filter(x => x === 'o').length;
+}
+
