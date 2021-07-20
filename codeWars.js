638d3bec046ab88133a9903df146cd40dc9cd941
Jaime Salgado <jsalg10@gmail.com>
2021-07-20 16:41:53 -0500

    daily challenge

--- codeWars.js
+++ codeWars.js
@@ -1523,3 +1523,22 @@ function filter_list(l) {
   return l.filter(x => typeof x == "number");
 }
 
+//daily challenge 
+
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+// Examples
+
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))((" 
+
+function duplicateEncode(word){
+  return word
+    .toLowerCase()
+    .split('')
+    .map( function (a, i, w) {
+      return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
+    })
+    .join('');
+}
