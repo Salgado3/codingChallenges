5909776055a588f2e28f80dae95bf72e0e62cd9d
Jaime Salgado <jsalg10@gmail.com>
2021-08-04 23:14:16 -0500

    daily challenge solution

--- codeWars.js
+++ codeWars.js
@@ -1627,3 +1627,26 @@ function findDeletedNumber(arr, mixArr) {
   return arr.find ((element) => {return !mixArr.includes (element)}) || 0;
 }
  //8/4/2021
+
+//  You need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+// The input to the function will be an array of three distinct numbers.
+
+// For example:
+
+// gimme([2, 3, 1]) => 0
+// 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+// Another example (just to make sure it is clear):
+
+// gimme([5, 10, 14]) => 1
+// 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+
+var gimme = function (arr) {
+
+  let middle = [...arr].sort((a,b)=> a-b).slice(1,2)  
+   
+   return arr.indexOf(middle[0])
+   
+ 
+ }
