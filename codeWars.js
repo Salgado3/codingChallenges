0d7d3b26a506e979e7c4855faaada67e031df65c
Jaime Salgado <jsalg10@gmail.com>
2021-09-08 22:27:05 -0500

    daily challenge

--- codeWars.js
+++ codeWars.js
@@ -1871,3 +1871,33 @@ function generateRange(min, max, step){
 function wordsToSentence(words) {
   return words.join(" ")
 }
+///9/8/2021
+
+// Complete the method which returns the number which is most frequent in the given input array. If there is a tie for most frequent number, return the largest number among them.
+
+// Note: no empty arrays will be given.
+// Examples
+
+// [12, 10, 8, 12, 7, 6, 4, 10, 12]              -->  12
+// [12, 10, 8, 12, 7, 6, 4, 10, 12, 10]          -->  12
+// [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3
+
+
+function highestRank(arr){
+  var obj = {};
+  arr.forEach(function(elem){
+     if(obj[elem] === undefined)
+       obj[elem] = 0;
+     obj[elem]++;
+  });
+  var keys = Object.keys(obj), highest = 0, key;
+  keys.forEach(function(elem){
+     if(obj[elem] >= highest)
+     {
+        highest = obj[elem];
+        key = elem;
+     }
+  })
+  return parseInt(key);
+  
+}
