fc9f84f1f33c4ecd290878532a91c7cb87a4426f
Jaime Salgado <jsalg10@gmail.com>
2021-09-10 08:08:05 -0500

    daily challenge

--- codeWars.js
+++ codeWars.js
@@ -1900,3 +1900,23 @@ function highestRank(arr){
   })
   return parseInt(key);
 }
+
+//9/10/2021
+
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+// For example:
+
+// a = 1
+// b = 4
+// --> [1, 2, 3, 4]
+
+function between(a, b) {
+  let arr = []
+  for(let i =a; i<=b; i++){
+ arr.push(i)
+}
+  return arr
+}
+
+
