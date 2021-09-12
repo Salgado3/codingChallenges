14f2f58d2c13cd5e5eff1c367806ed0c5ea0aeb1
Jaime Salgado <jsalg10@gmail.com>
2021-09-11 21:50:15 -0500

    daily challenge

--- codeWars.js
+++ codeWars.js
@@ -1919,4 +1919,20 @@ function between(a, b) {
   return arr
 }
 
+//Daily challenge
 
+// Description:
+
+// Remove all exclamation marks from the end of sentence.
+// Examples
+
+// remove("Hi!") === "Hi"
+// remove("Hi!!!") === "Hi"
+// remove("!Hi") === "!Hi"
+// remove("!Hi!") === "!Hi"
+// remove("Hi! Hi!") === "Hi! Hi"
+// remove("Hi") === "Hi"
+
+
+
+const remove = s => s.replace(/!+$/, '');
