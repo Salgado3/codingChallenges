a1c8abf60f25364a411a157a97d57071b897a45d
Jaime Salgado <jsalg10@gmail.com>
2021-09-11 22:33:24 -0500

    second challenge

--- codeWars.js
+++ codeWars.js
@@ -1936,3 +1936,18 @@ function between(a, b) {
 
 
 const remove = s => s.replace(/!+$/, '');
+
+//second problem
+
+// For every good kata idea there seem to be quite a few bad ones!
+
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+function well(x){
+  let pass = x.filter(e=> e=="good").length
+  
+  return pass >=2 ? 'I smell a series!':pass > 0? "Publish!":"Fail!"
+  
+  }
+
+
