8f8b757895ce48c3cc692ec910f6a1561714becf
Jaime Salgado <jsalg10@gmail.com>
2021-10-03 20:01:05 -0500

    daily challenge

--- codeWars.js
+++ codeWars.js
@@ -2150,3 +2150,20 @@ var smallerNumbersThanCurrent = function(nums) {
   };
   return newArr; 
 };
+// 10/3
+
+// Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+// Examples:
+
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+
+function longest(s1, s2) {
+  let newString = [...new Set(s1+s2)].sort().join("")
+ return newString
+ 
+ 
+ }
+ 
