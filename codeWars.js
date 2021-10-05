c7009c63aa600fb615b0b00437d6db1d5aa7659f
Jaime Salgado <jsalg10@gmail.com>
2021-10-04 22:33:46 -0500

    daily challenge

--- codeWars.js
+++ codeWars.js
@@ -2166,4 +2166,30 @@ function longest(s1, s2) {
  
  
  }
- 
+ //10/4/2021
+//  Your job is to implement a function which returns the last D digits of an integer N as a list.
+// Special cases:
+
+//     If D > (the number of digits of N), return all the digits.
+//     If D <= 0, return an empty list.
+
+// Examples:
+
+// N = 1
+// D = 1
+// result = [1]
+
+// N = 1234
+// D = 2
+// result = [3, 4]
+
+function lastDigit(n, d) {
+  if(d<=0){
+    return []
+  }
+    let nArr =[n].toString().split("").slice(-d).map(x=>Number(x))
+    
+    return nArr
+  }
+  
+   
