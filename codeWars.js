83c8396821e7fecf037f3bbb15edd87038182a9f
Jaime Salgado <jsalg10@gmail.com>
2021-10-06 15:59:31 -0500

    daily challenge

--- codeWars.js
+++ codeWars.js
@@ -2222,4 +2222,22 @@ function lastDigit(n, d) {
     
   }
     return revStr
+//10/6/2021
+// Triple Trouble
+// Create a function that will return a string that combines all of the letters of the three inputed strings in groups. Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below!
+
+// E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
+
+// Note: You can expect all of the inputs to be the same length.
+
+function tripleTrouble(one, two, three){
+  let newStr = ""
+  for(let i = 0; i <one.length; i++){
+    newStr+=one[i]
+  newStr+=two[i]
+  newStr+=three[i]
+    
   }
+  
+  return newStr
+   }
