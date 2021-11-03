668867920a83924b00940966f774f1d09b017327
Jaime Salgado <jsalg10@gmail.com>
2021-11-03 14:31:41 -0500

    daily challenge #2 The average length

--- codeWars.js
+++ codeWars.js
@@ -2748,4 +2748,23 @@ function sortArray(array) {
 return array.map(x => x%2!==0? oddSorted.shift():x)
 }
 
- 
+ ///11/3/2021
+//  The average length (code wars)
+//  Given an array of strings of the same letter type. Return a new array, which will differ in that the length of each element is equal to the average length of the elements of the previous array.
+
+// A few examples:
+
+// ['u', 'y'] =>  ['u', 'y'] // average length is 1
+// ['aa', 'bbb', 'cccc'] => ['aaa', 'bbb', 'ccc'] // average length is 3
+// ['aa', 'bb', 'ddd', 'eee'] => ['aaa', 'bbb', 'ddd', 'eee'] // average length is 2.5 round up to 3
+
+//     If the average length is not an integer, use Math.round().
+//     The input array's length > 1
+
+
+function averageLength(arr) { 
+  let average = Math.round(arr.map(x=> x.length).reduce((a,c)=> a+c,0)/arr.length)
+
+return arr.map(x=> x[0].repeat(average))
+}
+
