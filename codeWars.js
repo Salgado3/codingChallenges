20ad96fd0101482c466666b7bffbb25ea2881ac8
Jaime Salgado <jsalg10@gmail.com>
2021-11-11 10:02:42 -0600

    daily challenge LC 58 Length of Last Word

--- codeWars.js
+++ codeWars.js
@@ -2783,3 +2783,39 @@ return arr.map(x=> x[0].repeat(average))
 // If you like this Kata, please try:
 
 return arr.map(str=> str.split("").filter((x,index,arr)=> x!= arr[index+1]).join(""))
+
+//11/11
+
+// 58. Length of Last Word
+// Easy
+
+// Given a string s consisting of some words separated by some number of spaces, return the length of the last word in the string.
+
+// A word is a maximal substring consisting of non-space characters only.
+
+ 
+
+// Example 1:
+
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+
+// Example 2:
+
+// Input: s = "   fly me   to   the moon  "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+
+// Example 3:
+
+// Input: s = "luffy is still joyboy"
+// Output: 6
+// Explanation: The last word is "joyboy" with length 6.
+var lengthOfLastWord = function(s) {
+   
+  let arr = s.trim().split(" ")
+  
+   return arr[arr.length -1].length
+  };
+  
