37634e0c7e6233bf23a2153c10520d14cee857c0
Jaime Salgado <jsalg10@gmail.com>
2021-11-23 20:48:37 -0600

    daily challenge Leet Maximum Subarray

--- codeWars.js
+++ codeWars.js
@@ -2889,7 +2889,7 @@ function mergeArrays(a, b) {
     //11/19/2021 
     function count (string) {  
       let map = new Map() 
-    
+     
     if(string == "")
     return {}
     
@@ -2906,4 +2906,52 @@ function mergeArrays(a, b) {
     
     }
   
-    console.log(count("abadfefdfgd"))
+    console.log(count("abadfefdfgd")) 
+
+    // LeetCode 53. Maximum Subarray
+
+//     53. Maximum Subarray
+// Easy
+
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+// A subarray is a contiguous part of an array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: [4,-1,2,1] has the largest sum = 6.
+
+// Example 2:
+
+// Input: nums = [1]
+// Output: 1
+
+// Example 3:
+
+// Input: nums = [5,4,-1,7,8]
+// Output: 23
+
+ 
+
+// Constraints:
+
+//     1 <= nums.length <= 105
+//     -104 <= nums[i] <= 104
+
+
+const maxSubArray = function(arr) {
+  let currentSum = 0;
+  let maxSum = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+      if (currentSum < 0) {
+          currentSum = 0;
+      }
+      currentSum += arr[i];
+      maxSum = Math.max(currentSum, maxSum)
+  }
+  return maxSum;
+};
