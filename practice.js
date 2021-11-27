d5764dded28dad7bd70173d17fbe3879c9dea7e7
Jaime Salgado <jsalg10@gmail.com>
2021-11-27 11:20:59 -0600

    daily challenge

--- practice.js
+++ practice.js
@@ -433,23 +433,83 @@
 
 
 
-  function count (string) {  
-    let map = new Map() 
+  // function count (string) {  
+  //   let map = new Map() 
   
-  if(string == "")
-  return {}
+  // if(string == "")
+  // return {}
   
-    for(let i =0; i<string.length;i++){
-    if(map.has(string[i])){
-         map.get(string[i])++
-  }else {
-    map.set(string[i],1)
-  }
+  //   for(let i =0; i<string.length;i++){
+  //   if(map.has(string[i])){
+  //        map.get(string[i])++
+  // }else {
+  //   map.set(string[i],1)
+  // }
   
-  }
+  // }
    
-  return map.entries()
+  // return map.entries()
   
-  }
+  // }
 
-  console.log(count("abadfefdfgd"))
+  // console.log(count("abadfefdfgd"))
+
+  // 11/26
+
+ 
+
+// function maxMin(arr) {
+
+//   let sorted = arr.sort((a,b)=>a-b)
+//   let max = sorted[arr.length-1]
+//   let min = sorted[0]
+
+// return [max,min]
+// }
+
+// console.log(maxMin([1,2,41,43,2,56,23,455]))
+
+// ________________________________________________________________________
+/**
+* Given a pattern and a string s, find if s follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+
+ 
+Example 1:
+Input: pattern = "abba", s = "dog cat cat dog"
+Output: true
+
+Example 2:
+Input: pattern = "abba", s = "dog cat cat fish"
+Output: false
+
+Example 3:
+Input: pattern = "aaaa", s = "dog cat cat dog"
+Output: false
+
+Example 4:
+Input: pattern = "abba", s = "dog dog dog dog"
+Output: false
+
+ */
+
+const wordPattern = function(pattern, str) {
+    const words = str.split(" ");
+    const map = new Map();
+    
+    if(words.length !== pattern.length) return false;
+    if(new Set(words).size !== new Set(pattern).size) return false;
+
+    for(let i = 0; i < pattern.length; i++) {
+        if(map.has(pattern[i]) && 
+           map.get(pattern[i]) !== words[i]) return false;
+        map.set(pattern[i], words[i]);
+    }
+    return true;
+};
+
+console.log(wordPattern("abba","dog cat dog cat"))
+console.log(wordPattern("abba","dog dog dog dog"))
+
+console.log(wordPattern("abba","dog cat cat dog fish"))
