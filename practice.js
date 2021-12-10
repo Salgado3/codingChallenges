304c0953c1abecfd8dc7e1d63c5da5a78ca67cbc
Jaime Salgado <jsalg10@gmail.com>
2021-12-09 20:50:47 -0600

    2sum using an object

--- practice.js
+++ practice.js
@@ -901,14 +901,58 @@ c.right = f;
 
 // Euclids algo 
 
-let gcd = (a,b) => {
+// let gcd = (a,b) => {
 
-if(!b) {
-  return a
+// if(!b) {
+//   return a
+// }
+
+// return gcd(b, a%b)
+
+// }
+
+// console.log(gcd(1680,640))
+
+// Given an array of numbers, return all pairs that add up to a given sum. The numbers can be used more than once.
+
+// describe("Two Sum", () => {
+//  it("Should implement two sum", () => {
+//   assert.deepEqual(twoSum([1, 2, 2, 3, 4], 4), [[2, 2], [3, 1]]);
+//  });
+// });
+
+function twoSum (nums, sum) {
+    let map = new Map() 
+    let resultArr = []
+
+  for(let i=0; i<nums.length; i++){
+    let target = sum - nums[i]
+
+    if(!map.has(target)){
+      map.set(nums[i], true)
+    }else{
+        resultArr.push([target,nums[i]])
+   
+    }
+  }
+
+
+return resultArr
+
 }
 
-return gcd(b, a%b)
 
-}
 
-console.log(gcd(1680,640))
+var twoSum = function(nums, target) {
+  let map = new Map();
+  
+  for(let i = 0; i < nums.length; i ++) {
+      if(map.has(target - nums[i])) {
+          return [map.get(target - nums[i]), i];
+      } else {
+          map.set(nums[i], i);
+      }
+  }
+return [];
+};
+console.log(twoSum([1, 2, 2, 3, 4], 4))
