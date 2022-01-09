2c3fb0d34df1495541778c649b1d1685db077137
Jaime Salgado <jsalg10@gmail.com>
2022-01-08 23:29:44 -0600

    daily challenge. remove node from linked list

--- practice.js
+++ practice.js
@@ -1231,3 +1231,41 @@ return maxChar
 };
 
 console.log(mostFrequentChar('bookeeper'))
+//1/8/2022
+
+// remove node
+
+// Write a function, removeNode, that takes in the head of a linked list and a target value as arguments. The function should delete the node containing the target value from the linked list and return the head of the resulting linked list. If the target appears multiple times in the linked list, only remove the first instance of the target in the list.
+
+// Do this in-place.
+
+// You may assume that the input list is non-empty.
+
+// You may assume that the input list contains the target.
+
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+const removeNode = (head, targetVal) => {
+  if(head.val === targetVal) return head.next
+         let previous = null
+         let current = head
+  
+  
+      while(current!==null){
+        if(current.val === targetVal) {
+          prev.next = current.next
+           break;
+         }
+             prev = current
+           current = current.next
+      }
+  
+    return head
+  };
+  
+  
