147651d63226788ff9845f7ffe1c42ca82a2d8ad
Jaime Salgado <jsalg10@gmail.com>
2022-01-24 22:05:57 -0600

    daily challenge

--- practice.js
+++ practice.js
@@ -1604,3 +1604,22 @@ const maxArea = function(height) {
  }
   return area
 };
+
+
+
+
+
+const numIdenticalPairs = function(nums) {
+  const map = {}
+  let count = 0
+  
+  for (const number of nums) {
+      if (map[number]) {
+          count += map[number];
+          map[number] += 1;
+      } else {
+          map[number] = 1;
+      }
+  }
+  return count
+};
