65ce49abb169feeefda7e418211386c9c1554194
Jaime Salgado <jsalg10@gmail.com>
2022-03-23 23:54:09 -0500

    daily challenge. Wes Bos 30

--- practiceText.js
+++ practiceText.js
@@ -1,29 +1,116 @@
-createPost: async (req, res) => {
-  if (Post.findOne({ activityName: req.body.activityName })) {
-    try {
-      await Post.findOneAndUpdate(
-        { activityName: req.body.activityName },
-        {
-          $inc: { time: Number(req.body.time) },
-        }
-      );
-      console.log("Likes +1");
-      res.redirect(`/feed`);
-    } catch (err) {
-      console.log(err);
-    }
-  } else {
-    try {
-      await Post.create({
-        activityName: req.body.activityName,
-        time: Number(req.body.time),
+// Get your shorts on - this is an array workout!
+// ## Array Cardio Day 1
 
-        user: req.user.id,
-      });
-      console.log("Post has been added!");
-      res.redirect("/feed");
-    } catch (err) {
-      console.log(err);
-    }
-  }
-};
+// Some data we can work with
+
+const inventors = [
+  { first: "Albert", last: "Einstein", year: 1879, passed: 1955 },
+  { first: "Isaac", last: "Newton", year: 1643, passed: 1727 },
+  { first: "Galileo", last: "Galilei", year: 1564, passed: 1642 },
+  { first: "Marie", last: "Curie", year: 1867, passed: 1934 },
+  { first: "Johannes", last: "Kepler", year: 1571, passed: 1630 },
+  { first: "Nicolaus", last: "Copernicus", year: 1473, passed: 1543 },
+  { first: "Max", last: "Planck", year: 1858, passed: 1947 },
+  { first: "Katherine", last: "Blodgett", year: 1898, passed: 1979 },
+  { first: "Ada", last: "Lovelace", year: 1815, passed: 1852 },
+  { first: "Sarah E.", last: "Goode", year: 1855, passed: 1905 },
+  { first: "Lise", last: "Meitner", year: 1878, passed: 1968 },
+  { first: "Hanna", last: "Hammarström", year: 1829, passed: 1909 },
+];
+
+const people = [
+  "Bernhard, Sandra",
+  "Bethea, Erin",
+  "Becker, Carl",
+  "Bentsen, Lloyd",
+  "Beckett, Samuel",
+  "Blake, William",
+  "Berger, Ric",
+  "Beddoes, Mick",
+  "Beethoven, Ludwig",
+  "Belloc, Hilaire",
+  "Begin, Menachem",
+  "Bellow, Saul",
+  "Benchley, Robert",
+  "Blair, Robert",
+  "Benenson, Peter",
+  "Benjamin, Walter",
+  "Berlin, Irving",
+  "Benn, Tony",
+  "Benson, Leana",
+  "Bent, Silas",
+  "Berle, Milton",
+  "Berry, Halle",
+  "Biko, Steve",
+  "Beck, Glenn",
+  "Bergman, Ingmar",
+  "Black, Elk",
+  "Berio, Luciano",
+  "Berne, Eric",
+  "Berra, Yogi",
+  "Berry, Wendell",
+  "Bevan, Aneurin",
+  "Ben-Gurion, David",
+  "Bevel, Ken",
+  "Biden, Joseph",
+  "Bennington, Chester",
+  "Bierce, Ambrose",
+  "Billings, Josh",
+  "Birrell, Augustine",
+  "Blair, Tony",
+  "Beecher, Henry",
+  "Biondo, Frank",
+];
+
+// Array.prototype.filter()
+// 1. Filter the list of inventors for those who were born in the 1500's
+console.log(inventors.filter((x) => x.year > 1499 && x.year < 1600));
+// Array.prototype.map()
+// 2. Give us an array of the inventors first and last names
+console.log(inventors.map((name) => name.first + "," + name.last));
+// Array.prototype.sort()
+// 3. Sort the inventors by birthdate, oldest to youngest
+console.log(inventors.sort((a, b) => a.year - b.year));
+// Array.prototype.reduce()
+// 4. How many years did all the inventors live all together?
+console.log(
+  inventors.reduce((a, c) => {
+    return a + (c.passed - c.year);
+  }, 0)
+);
+// 5. Sort the inventors by years lived
+console.log(inventors.sort((a, b) => b.passed - b.year - (a.passed - a.year)));
+// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
+// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+
+// 7. sort Exercise
+// Sort the people alphabetically by last name
+
+// 8. Reduce Exercise
+// Sum up the instances of each of these
+const data = [
+  "car",
+  "car",
+  "truck",
+  "truck",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "car",
+  "truck",
+];
+
+let sumOfInstances = {};
+for (let vehicle of data) {
+  if (sumOfInstances[vehicle]) sumOfInstances[vehicle] += 1;
+  else sumOfInstances[vehicle] = 1;
+}
+
+console.log(sumOfInstances);
+
+console.log(people.sort());
