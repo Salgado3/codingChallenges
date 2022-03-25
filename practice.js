f196f35345a45ce498b8191d2f1aea41f09c45df
Jaime Salgado <jsalg10@gmail.com>
2022-03-24 23:13:09 -0500

    daily challenge

--- practice.js
+++ practice.js
@@ -2321,3 +2321,58 @@ var climbStairs = function(n) {
      }
      return one
  };
+
+ /// 3/24
+//  substituting synonyms
+
+// Write a function, substitutingSynonyms, that takes in a sentence and an object as arguments. The object contains words as keys whose values are arrays containing synonyms. The function should return an array containing all possible sentences that can be formed by substituting words of the sentence with their synonyms.
+
+// You may return the possible sentences in any order, as long as you return all of them.
+// test_00:
+
+// const sentence = "follow the yellow brick road";
+// const synonyms = {
+//   follow: ["chase", "pursue"],
+//   yellow: ["gold", "amber", "lemon"],
+// };
+
+// substituteSynonyms(sentence, synonyms);
+// [
+//   'chase the gold brick road',
+//   'chase the amber brick road',
+//   'chase the lemon brick road',
+//   'pursue the gold brick road',
+//   'pursue the amber brick road',
+//   'pursue the lemon brick road'
+// ]
+
+const substituteSynonyms = (sentence, synonyms) => {
+  const words = sentence.split(" ")
+  const arrays = generate(words, synonyms)
+  return arrays.map(subarray => subarray.join(" "))
+};
+
+
+const generate = (words, synonyms) => {
+  if(words.length === 0) return [[]]
+  
+  const firstWord = words[0]
+  const remainingWords = words.slice(1)
+  
+  if(firstWord in synonyms) {
+    const result = []
+    const subarrays = generate(remainingWords, synonyms)
+   for(let synonym of synonyms[firstWord]) {
+     result.push(...subarrays.map(subarray=> [synonym, ...subarray]))
+   }
+    return result
+  } else {
+    const subarrays = generate(remainingWords, synonyms)
+    return subarrays.map(subarray=> [firstWord, ...subarray])
+  }
+   
+}
+
+
+
+
